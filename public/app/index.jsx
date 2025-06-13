6441c9fce348847845444b9da39d509acc26553f
Louie <louigiecads143@gmail.com>
2025-06-13 13:34:10 +0800

    Frontend Environment Setup

---
+++ public/app/index.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home () {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
